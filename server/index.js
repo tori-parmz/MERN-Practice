6bda9a20300c7895d4928083925aec40f8bff6ca
tori-parmz <119701826+tori-parmz@users.noreply.github.com>
2023-07-05 13:17:31 -0500

    starting to add express server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,18 @@
 // MERN = Mongo + Express + REact + Node
 
 //Development = Node.js server + React server
+
+// MEN
+
+// E - Express
+
+import express from 'express'
+const app = express()
+
+app.get('/hello', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(1337, () => {
+    console.log('Server started on 1337')
+})
