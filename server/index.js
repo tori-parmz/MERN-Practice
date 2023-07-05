9ae66c520bd0f9020c8b2626e89bccdf48318256
tori-parmz <119701826+tori-parmz@users.noreply.github.com>
2023-07-05 15:59:47 -0500

    first time using mongodb

--- server/index.js
+++ server/index.js
@@ -9,12 +9,37 @@
 import express from 'express';
 const app = express();
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 app.use(cors())
+app.use(express.json())
 
-app.post('/api/register', (req, res) => {
+mongoose.connect('mongodb://localhost:27017/mern-stack-practice')
+
+app.post('/api/register', async (req, res) => {
     console.log(req.body)
-    res.json({ status: 'ok '})
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({ status: 'ok' })
+    } catch (err) {
+        res.json({ status: 'error', error: 'Duplicate email' })
+    }
+})
+
+app.post('/api/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            email: req.body.email,
+            password: req.body.password,
+        })
+        res.json({ status: 'ok' })
+    } catch (err) {
+        res.json({ status: 'error', error: 'Duplicate email' })
+    }
 })
 
 app.listen(1337, () => {
